427f98c9ebd037cb22e300a2e83aabf42cf73a98
Idle0x <140549997+Idle0x@users.noreply.github.com>
2026-01-02 12:57:54 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { FinancialProvider } from './context/FinancialContext'; // Import this
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

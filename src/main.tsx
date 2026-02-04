050ebf900895e25f165d2a063a80a5afbe3535f2
Idle0x <140549997+Idle0x@users.noreply.github.com>
2026-02-04 05:27:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,29 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { FinancialProvider } from './context/FinancialContext'; // Import this
+import App from './App';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UserProvider } from './context/UserContext';
+import { LedgerProvider } from './context/LedgerContext';
+
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // Data is fresh for 5 minutes
+      retry: 1,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FinancialProvider> {/* Wrap App */}
-      <App />
-    </FinancialProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <LedgerProvider>
+          <App />
+        </LedgerProvider>
+      </UserProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
